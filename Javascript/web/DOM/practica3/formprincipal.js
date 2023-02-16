54bdb854a233fe81154d44d37fa09e48f7240dd9
daniel2936 <danielvillamizar72@gmail.com>
2023-02-16 13:49:01 -0500

    24  commit

--- Javascript/web/DOM/practica3/formprincipal.js
+++ Javascript/web/DOM/practica3/formprincipal.js
@@ -1,22 +1,44 @@
 const btn=document.querySelector('#submit')
+const cajagrande=document.getElementById('contenedor')
+
+
+
+// btn.addEventListener('click',function(){
+
+//     const nombre=document.querySelector('#nombre').value
+//     const apellido=document.querySelector('#apellidos').value
+//     const edad=document.querySelector('#edad').value
+//     const fecha=document.querySelector('#fechanac').value
+//     const salario=document.querySelector('#salario').value
+//     const objeto={
+
+//         nombre,
+//         apellido,
+//         edad,
+//         fecha,
+//         salario
+
+
+//     }
+
+//     console.log(objeto)
+// })
+
+
 
 btn.addEventListener('click',function(){
-
-    const nombre=document.querySelector('#nombre').value
-    const apellido=document.querySelector('#apellidos').value
-    const edad=document.querySelector('#edad').value
-    const fecha=document.querySelector('#fechanac').value
-    const salario=document.querySelector('#salario').value
-    const objeto={
-        
-        nombre,
-        apellido,
-        edad,
-        fecha,
-        salario
+    const nombre= document.querySelector('#nombre').value
+    const apellido= document.querySelector('#apellidos').value
+    const salario= document.querySelector('#salario').value
 
 
-    }
+     cajagrande.innerHTML+=`<div id="caja">
+        <div><img class="circol"  src="./assets/descarga.png" alt=""></div>
+        <p id="nome">${nombre}</p>
+        <p id="ape">${apellido}</p>
+        <p id="sala">${salario}</p>
+    </div>`
 
-    console.log(objeto)
+        document.getElementById('formulario').reset()
+       
 })
